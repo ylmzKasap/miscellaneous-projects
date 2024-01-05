33fc4963f27d10853258cb9cc5664ca3b7f16317
Yılmaz Kasap <ylmzkasap@outlook.com>
2024-01-05 10:13:46 +0300

    Fix wifiName foreign character issue

--- wifi-grabber/wifi-grabber.py
+++ wifi-grabber/wifi-grabber.py
@@ -1,11 +1,8 @@
-# Displays all wifi names and passwords saved to a Windows device.
-
 import subprocess
 import re
 import os
 
 wifiNamePasswordDict = {}
-wifiNameRegex = re.compile(r'(?<=All User Profile     : )[\w\s\d#-\'\"@$€|%&.,~₺<!?>*+:;-]+(?=\\r\\n)')
 passwordRegex = re.compile(r'(?<=Key Content            : )[\w\s\d#-\'\"@$€|%&.,~₺<!?>*+:;-]+(?=\\r\\n)')
 
 print('\nWorking on it...')
@@ -20,9 +17,22 @@ getWifiNames = str(subprocess.run(
     ['netsh', 'wlan', 'show', 'profiles'],
     stdout=subprocess.PIPE, shell=True).stdout)
 
-wifiNamesList = wifiNameRegex.findall(getWifiNames)
+wifiNamesList = (getWifiNames.replace('\\r', '')
+    .replace('\\n', '')
+    .replace('\\x99', 'Ö')
+    .replace('\\x94', 'ö')
+    .replace('\\x9f', 'ş')
+    .replace('\\x9e', 'Ş')
+    .replace('\\x87', 'ç')
+    .replace('\\x80', 'Ç')
+    .replace('\\xa7', 'ğ')
+    .replace('\\xa6', 'Ğ')
+    .replace('\\x98', 'İ')
+    .replace('\\x8d', 'ı')
+    .split('All User Profile     :')[1:])
 
 for wifiName in wifiNamesList:
+    wifiName = wifiName.strip()
     getPassword = str(subprocess.run(
         ['netsh', 'wlan', 'show', 'profile', wifiName, 'key=clear'],
         stdout=subprocess.PIPE, shell=True).stdout)
